b1d0079949559fbabb4c69293f22f29253c82a6e
Kevin Warui <waruimacharia@gmail.com>
2024-04-18 04:22:40 -0700

    Refactor 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -12,6 +12,21 @@ if (process.argv.length !== 3 || !Number.isInteger(Number(process.argv[2]))) {
 const filmId = process.argv[2];
 const filmUrl = `https://swapi-api.alx-tools.com/api/films/${filmId}`;
 
+// get character name from person url
+const getCharacterName = (url) => {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) {
+        reject(error);
+        return;
+      }
+
+      const characterData = JSON.parse(body);
+      resolve(characterData.name);
+    });
+  });
+};
+
 request(filmUrl, (error, response, body) => {
   if (error) {
     console.error('Error:', error);
@@ -27,15 +42,13 @@ request(filmUrl, (error, response, body) => {
   const filmData = JSON.parse(body);
 
   // get name of each actor
-  for (const character of filmData.characters) {
-    request(character, (error, response, body) => {
-      if (error) {
-        console.error('Error:', error);
-        return;
-      }
+  const promises = filmData.characters.map(getCharacterName);
 
-      const characterData = JSON.parse(body);
-      console.log(characterData.name);
+  Promise.all(promises)
+    .then(names => {
+      names.forEach(name => console.log(name));
+    })
+    .catch(error => {
+      console.error('Error:', error);
     });
-  }
 });
