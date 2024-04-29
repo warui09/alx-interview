c9ad7a948457b0121c19fe6e87a7659ed234d032
Kevin Warui <waruimacharia@gmail.com>
2024-04-29 10:09:46 +0300

    Add 0-making_change.py

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Task 0 solution"""
+
+
+def makeChange(coins, total):
+    """Finds smallest number of coins to make change"""
+
+    if total <= 0:
+        return 0
+
+    coins.sort(reverse=True)
+    total_coins = 0
+
+    for coin in coins:
+        num_coins = 0
+        if total > coin:
+            num_coins = total // coin
+            total_coins += num_coins
+            total -= coin * num_coins
+
+    if total == 0:
+        return total_coins
+    return -1
