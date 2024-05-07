22a42fd6975ec5607c901c5a19b418d434a7b4e2
Kevin Warui <waruimacharia@gmail.com>
2024-05-07 09:08:13 +0300

    Refactor 0-island_perimeter.py

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -8,25 +8,14 @@ def island_perimeter(grid):
     perimeter = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
-            neighbors = 0
             if grid[i][j] == 1:
-                # get neighbors
-                if j > 0 and grid[i][j - 1] == 1:
-                    neighbors += 1
-                if i > 0 and grid[i - 1][j] == 1:
-                    neighbors += 1
-                if j < len(grid[i]) - 2 and grid[i][j + 1] == 1:
-                    neighbors += 1
-                if i < len(grid) - 2 and grid[i + 1][j] == 1:
-                    neighbors += 1
-
-                if neighbors == 0:
-                    perimeter += 4
-                elif neighbors == 1:
-                    perimeter += 3
-                elif neighbors == 2:
-                    perimeter += 2
-                elif neighbors == 3:
+                if grid[i][j - 1] == 0:
+                    perimeter += 1
+                if grid[i - 1][j] == 0:
+                    perimeter += 1
+                if grid[i][j + 1] == 0:
+                    perimeter += 1
+                if grid[i + 1][j] == 0:
                     perimeter += 1
 
     return perimeter
